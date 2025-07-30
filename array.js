ba0ebed425c8f5d7307d5b7875ef7fdf460a1075
Shafiur Rahman Rad <shafiur@S.R.Rad>
2025-07-30 22:51:52 +0600

    second commit

---
+++ array.js
@@ -0,0 +1,10 @@
+const friends=['rad', 'gadd', 'chad', 'daad']
+
+console.log(friends[2])
+
+
+console.log(friends.shift())
+
+friends.unshift('chauhd', 'uhusbf', 'zuhsdhf', 'iysd')
+
+console.log(friends)
